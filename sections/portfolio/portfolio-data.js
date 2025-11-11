3899d571ec78af48522b88eb02f1a3d56369b89c
mojeed-painless <145933393+mojeed-painless@users.noreply.github.com>
2025-11-11 21:54:41 +0100

    Refactor portfolio images to use imports

--- sections/portfolio/portfolio-data.js
+++ sections/portfolio/portfolio-data.js
@@ -1,11 +1,12 @@
-
+import portfolioImage1 from '../../src/assets/portfolio1.jpg';
+import portfolioImage2 from '../../src/assets/portfolio2.jpg';
 
 const data = [
     {
         id: 1, 
         category: 'Frontend', 
         title: 'Project One', 
-        image: '../../src/assets/portfolio1.jpg',
+        image: portfolioImage1,
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi facilis iure quaerat minus maxime, ratione velit. Ea ipsa harum similique amet consectetur adipisicing elit.',
         demoLink: '#', 
         codeLink: '#'
@@ -14,7 +15,7 @@ const data = [
         id: 2, 
         category: 'Backend',
         title: 'Project Two', 
-        image: '../../src/assets/portfolio2.jpg', 
+        image: portfolioImage2, 
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi facilis iure quaerat minus maxime, ratione velit. Ea ipsa harum similique amet consectetur adipisicing elit.',
         demoLink: '#',
         codeLink: '#'
@@ -23,7 +24,7 @@ const data = [
         id: 3, 
         category: 'UI/UX Design',
         title: 'Project Three', 
-        image: '../../src/assets/portfolio1.jpg',
+        image: portfolioImage1,
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi facilis iure quaerat minus maxime, ratione velit. Ea ipsa harum similique amet consectetur adipisicing elit.',
         demoLink: '#',
         codeLink: '#'
@@ -32,7 +33,7 @@ const data = [
         id: 4,
         category: 'Frontend',
         title: 'Project Four',
-        image: '../../src/assets/portfolio1.jpg',
+        image: portfolioImage1,
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi facilis iure quaerat minus maxime, ratione velit. Ea ipsa harum similique amet consectetur adipisicing elit.',
         demoLink: '#',
         codeLink: '#'
@@ -41,7 +42,7 @@ const data = [
         id: 5,
         category: 'Backend',
         title: 'Project Five',
-        image: '../../src/assets/portfolio1.jpg',
+        image: portfolioImage1,
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi facilis iure quaerat minus maxime, ratione velit. Ea ipsa harum similique amet consectetur adipisicing elit.',
         demoLink: '#',
         codeLink: '#'
@@ -50,7 +51,7 @@ const data = [
         id: 6, 
         category: 'UI/UX Design',
         title: 'Project Six',
-        image: '../../src/assets/portfolio1.jpg',
+        image: portfolioImage1,
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi facilis iure quaerat minus maxime, ratione velit. Ea ipsa harum similique amet consectetur adipisicing elit.',
         demoLink: '#',
         codeLink: '#'
